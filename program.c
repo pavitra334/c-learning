066b945bbb1a1621d290608e3a9e5a9e11253300
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2019-10-16 19:19:34 +0530

    update program.c

--- program.c
+++ program.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 
-int main()
-
-{
-  int n1=0, n2=1, i, n3, number;
-
-  printf("Enter the number of elements:\n");
-  scanf("%d", &number);
-
-  printf("%d\n%d\n", n1, n2 );
-
-
-  for (i=0; i<number; i++)
-   {
-
-    n3=n1+n2;
-     n1=n2;
-     n2=n3;
-
-
-
-       printf("%d\n", n3 );
-    /* code */
-  }
-  return 0;
-}
+int main(){
+    int fib_number_limit, number1=0, number2=1,itr, number3;
+    printf("Enter the number of elements:\n");
+    scanf("%d", &fib_number_limit);
+    printf("%d\n%d\n", number1, number2);
+    for ( itr = 0; itr < fib_number_limit; itr++){
+        number3 = number1 + number2;
+        number1 = number2;
+        number2 = number3;
+        printf("%d\n", number3 );
+        }
+    return 0;
+    }
